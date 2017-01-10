2831057ce3d599a80bfdde33ba638121623d14bc
wiyoo <ezone1@gmail.com>
2017-01-10 13:18:19 +0900

    test

--- gh_test/app/src/main/java/com/example/py/gh_test/MainActivity.java
+++ gh_test/app/src/main/java/com/example/py/gh_test/MainActivity.java
@@ -58,8 +58,6 @@ public class MainActivity extends Activity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
             case 2:
                 mTitle = getString(R.string.title_section2);
                 break;
