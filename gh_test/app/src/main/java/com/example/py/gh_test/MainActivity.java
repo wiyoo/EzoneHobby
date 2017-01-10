0480a00de91c9217d1aa985011ce93b640088ee8
wiyoo <ezone1@gmail.com>
2017-01-10 13:45:57 +0900

    test rebase

--- gh_test/app/src/main/java/com/example/py/gh_test/MainActivity.java
+++ gh_test/app/src/main/java/com/example/py/gh_test/MainActivity.java
@@ -64,8 +64,6 @@ public class MainActivity extends Activity
                 mTitle = getString(R.string.title_section2);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
             case 4:
                 mTitle = getString(R.string.title_section4);
                 break;
